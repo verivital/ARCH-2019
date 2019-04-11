dac51bf8e9eb24056e7c1941acb5f436c666a7b1
dieman95 <diego.manzanas.lopez@vanderbilt.edu>
2019-04-11 12:34:19 -0500

    Added controller ACC (tanh) and modified mountain car

--- Mountain_car/dynamicsMC.m
+++ Mountain_car/dynamicsMC.m
@@ -3,11 +3,7 @@ function [dx] = dynamicsMC(t,x,u)
 % x(1) = position
 % x(2) = velocity
 
-% Originally the discrete dynamics were taken from here
-% https://perma.cc/6Z2N-PFWC, where the time step is 0.5 seconds
-% So, we need to divide our dynamics by 0.5 to have them in continuous time
-
-dx(1,1) = 2*x(2);
-dx(2,1) = 0.003*u-0.005*cos(3*x(1));
+dx(1,1) = x(2);
+dx(2,1) = 0.0015*u-0.0025*cos(3*x(1));
 end
 
